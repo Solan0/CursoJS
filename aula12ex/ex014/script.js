c63c6fab50f1231de5ed284b92edb4dbd4fc0d7a
Solan0 <solano532@gmail.com>
2022-09-20 15:13:35 -0300

    site dinâmico

---
+++ aula12ex/ex014/script.js
@@ -0,0 +1,17 @@
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >=0 && hora<=12){
+        img.src='fotomanha.png'
+        document.body.style.background = '#309fed'
+    } else if (hora>12 && hora<=18){
+        img.scr = 'fototarde.png'
+        document.body.style.background = '#ff9401'
+    } else {
+        img.src = 'fotonoite.png'
+        document.body.style.background = '#000b11'
+    }
+}
